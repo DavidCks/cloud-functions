f4eaa5ea447cd821b17c9539b7e6de096c9effa2
DavidCks <david.christ99@live.de>
2023-02-18 00:43:12 +0900

    aaa

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import express from "npm:express@4.18.2";
-import axiod from "https://deno.land/x/axiod/mod.ts";
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
-
 app.all('*', handleRouting);
-//a
+
 function handleRouting(req, res) {
-	if (req.method === 'POST' || "GET" && req.originalUrl === '/trigger_deploy') {
-		const url = `${Deno.env.GITHUB_REPOSITORY_URL || "https://github.com/DavidCks/DavidCks.github.io.git"}`;
+	if (req.method === 'POST' && req.originalUrl === '/trigger_deploy') {
+		const url = `${process.env.GITHUB_REPOSITORY_URL || "https://github.com/DavidCks/DavidCks.github.io"}`;
 		const body = { event_type: 'trigger_deploy' };
 		const options = {
 			headers: {
 				Accept: 'valueapplication/vnd.github.v3+json',
 				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Deno.env.GITHUB_TOKEN || "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"}`,
+				Authorization: `Bearer ${process.env.GITHUB_TOKEN || "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"}`,
 			},
 		};
-		axiod.post(url, body, options);
+		axios.post(url, body, options);
 		res.send('Deployment triggered');
 	} else {
 		res.sendStatus(403);
 	}
 }
 
-app.listen(Deno.env.PORT || 2345, () => console.log(`Server started on port ${Deno.env.PORT || 2345} `));
+app.listen(process.env.PORT || 2345, () => console.log(`Server started on port ${process.env.PORT || 2345} `));
