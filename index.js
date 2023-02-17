42cda4596b2c6e4088c74eb37192da982d5fd757
DavidCks <david.christ99@live.de>
2023-02-18 01:58:56 +0900

    af

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ app.all('*', handleRouting);
 function handleRouting(req, res) {
 	if (req.method === 'POST' || 'GET' && req.originalUrl === '/trigger_deploy') {
 		const url = "https://api.github.com/repos/DavidCks/DavidCks.github.io/dispatches";
-		const body = { event_type: 'deploy', client_payload: {unit: false, integration:true}};
+		const body = {event_type:'deploy',client_payload:{unit:false,integration:true}};
 		const options = {
 			headers: {
 				Accept: 'application/vnd.github+json',
-				'Content-Type': 'application/json',
 				'X-GitHub-Api-Version': "2022-11-28",
-				Authorization: `Bearer ${process.env.GITHUB_TOKEN || "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"}`,
+				Authorization: 'Bearer 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',
 			},
 		};
 		axios.post(url, body, options);
